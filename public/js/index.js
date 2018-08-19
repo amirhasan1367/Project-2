3baf392a3db16c5869342d35f9a73f41eb29ce75
Amir PC <amirhasankntu@gmail.com>
2018-08-19 02:12:51 -0400

    some queue update

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,14 @@
 var $exampleDescription = $("#example-description"); */
 
 var $submitBtn = $("#submit");
+var $checkIn = $("#checkIn");
 /* var $exampleList = $("#example-list"); */
 
 var $ticketEntry = $("#ticketEntry")
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -19,8 +20,8 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-
-  ticketUpdate: function(ticket) {
+  //My code ----------------------------------------
+  ticketUpdate: function (ticket) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -30,13 +31,25 @@ var API = {
       data: JSON.stringify(ticket)
     });
   },
-  getExamples: function() {
+  rideCheckin: function (ride) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/ridescheckin",
+      data: JSON.stringify(ride)
+    });
+  },
+  //My code ----------------------------------------
+
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -75,7 +88,7 @@ var API = {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var ticket = {
@@ -83,14 +96,14 @@ var handleFormSubmit = function(event) {
     used: true
   };
 
-  if (!(ticket.ticket_no )) {
+  if (!(ticket.ticket_no)) {
     alert("Please enter a valid ticket number!");
     return;
   }
 
   $ticketEntry.val("");
 
-  API.ticketUpdate(ticket).then(function() {
+  API.ticketUpdate(ticket).then(function () {
     console.log("your entry is submitted, wait for your turn")
     window.location.href = "http://localhost:5000/rides";
 
@@ -98,19 +111,36 @@ var handleFormSubmit = function(event) {
 
 };
 
+var handleRideCheckin = function (event) {
+  event.preventDefault();
+
+  var queue = {
+    rideId: $("#checkIn").value,
+  };
+  console.log("what is the value: " + parseInt($(this).value));
+
+  API.rideCheckin(queue).then(function () {
+    console.log("you're in queue'")
+    //window.location.href = "http://localhost:5000/rides";
+
+  });
+
+};
+
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
+$checkIn.on("click", handleRideCheckin)
 /* $exampleList.on("click", ".delete", handleDeleteBtnClick);
  */
